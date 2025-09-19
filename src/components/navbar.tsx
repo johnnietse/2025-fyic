5b25447cbf472810ba9e5b065c72ae7959fc3f6e
Johnnie Tse <138593553+johnnietse@users.noreply.github.com>
2025-09-19 13:51:12 -0400

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -128,6 +128,7 @@ export function Navbar() {
             <img 
               src="/image/fyic-logo-cropped.png" 
               alt="FYIC Logo" 
+            />
 {/*               className="h-[90%] w-[90%] object-contain"
             /> */}
           </div>
