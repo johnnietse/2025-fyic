c78984b67d713dc561d96f6aa8da1472adec2e16
Johnnie Tse <138593553+johnnietse@users.noreply.github.com>
2025-09-19 00:38:42 -0400

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,8 +15,6 @@ const SOCIAL_LINKS = [
   {icon: "fa-brands fa-instagram", url: "https://www.instagram.com/fyic2025/"},
   {icon: "fa-solid fa-envelope", url: "mailto:fyic@engsoc.queensu.ca" } // email link
 
-  // {icon: "fa-brands fa-linkedin", url: "https://linkedin.com/" },
-  // {icon: "fa-brands fa-facebook", url: "https://facebook.com/" }
 
 ];
 
